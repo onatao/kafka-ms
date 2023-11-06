f57da71ff1a67d9ad9269ecb77b8628cbda68730
Nathan <nathanbarrosnt@gmail.com>
2023-11-06 13:37:33 -0300

    feat: base files

---
+++ src/test/java/com/neidev/ms/MsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neidev.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
